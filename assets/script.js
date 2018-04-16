033de1e473fd79ddfa549fa1b375be15a1767dcd
Michal Muszynski <m.muszy@gmail.com>
2018-04-16 22:44:37 +0200

    JS API and converter added

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,49 @@
-$(document).ready(function() {
+$(function() {
+    var $location = $(".location");
+    var $pressure = $(".pressure");
+    var $temperature = $(".temperature");
+    var $conditions = $(".conditions");
+    var $weather_icon = $(".weather-icon");
+    var $switch = $(".switch");
+    var tempF = 0;
+    var tempC = 0;
+    var tempA = 0;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             var lat = position.coords.latitude;
             var long = position.coords.longitude;
             var getIP ='https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=de66a6659e14650907b5cf92ffde9e62';
+
             $.getJSON(getIP, function (json) {
-                $(".location").html(json["name"]);
-                $(".pressure").html(json["main"]["pressure"]);
-                $(".temperature").html(json["main"]["temp"]);
-                $(".conditions").html(json["weather"][0]["main"]);
-                insertLogo();
+                var icon = json["weather"][0]["icon"];
+                var icon_address = 'https://openweathermap.org/img/w/'+ icon + '.png';
+                tempC = (json["main"]["temp"]-273.15);
+                $temperature.html(tempC);
+                tempF = Math.ceil((tempC*9)/5 + 32);
+                $location.html(json["name"]);
+                $pressure.html(json["main"]["pressure"]);
+                $conditions.html(json["weather"][0]["main"]);
+                $weather_icon.attr("src", icon_address);
+
             });
         });
     }
+
+    function switchUnits(event) {
+        event.preventDefault();
+        return((tempC*9)/5+-459.67);
+    }
+
+    function toggleTemp(event) {
+        event.preventDefault();
+        $temperature.html(($temperature).html() == tempC ? tempF : tempC)
+
+    }
+
+
+    $switch.on("click", toggleTemp)
 });
 
-function insertLogo() {
-    var element = document.getElementsByClassName("fas fa-sun")
-}
 
 
 
