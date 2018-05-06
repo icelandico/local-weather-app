a578cb253d4a2880ca329efd9d2cf9b3bc88edfd
Michal Muszynski <m.muszy@gmail.com>
2018-05-06 12:58:06 +0200

    Style enhanced, code refined

--- assets/script.js
+++ assets/script.js
@@ -7,42 +7,53 @@ $(function() {
     var $wind = $(".wind");
     var $temperature = $(".temperature");
     var $conditions = $(".conditions");
-    var $weather_icon = $(".weather-icon");
+    var $weather_icon = $("#weather-icon");
     var $switch = $(".switch");
-    var tempF = 0;
-    var tempC = 0;
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            $main.css({'display' : 'flex'});
-            $location_finding.css({'display' : 'none'});
+        navigator.geolocation.getCurrentPosition(function (position) {
+            $main.css({'display': 'flex'});
+            $location_finding.css({'display': 'none'});
             var lat = position.coords.latitude;
             var long = position.coords.longitude;
-            var getIP ='https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=de66a6659e14650907b5cf92ffde9e62';
-
+            var getIP = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=de66a6659e14650907b5cf92ffde9e62';
+            var getIcon = 'https://api.darksky.net/forecast/1678a0cbba8712f020e8c1129787ab9a/54,18';
             $.getJSON(getIP, function (json) {
-                var icon = json["weather"][0]["icon"];
-                var icon_address = 'https://openweathermap.org/img/w/'+ icon + '.png';
-                tempC = Math.round((json["main"]["temp"]-273.15));
-                $temperature.html((tempC));
-                tempF = Math.round((((tempC*9)/5 + 32))) ;
+                tempC = Math.round((json["main"]["temp"] - 273.15));
+                tempCIn = tempC + "°C";
+                $temperature.html((tempCIn));
+                tempF = Math.round((((tempC * 9) / 5 + 32)));
+                tempFIn = tempF + "°F";
                 $location.html(json["name"]);
                 $pressure.html(json["main"]["pressure"] + " hPa");
                 $humidity.html(json["main"]["humidity"] + " %");
                 $wind.html(json["wind"]["speed"] + " m/s");
                 $conditions.html(json["weather"][0]["main"]);
-                $weather_icon.attr("src", icon_address);
-            });
+
+                $.ajax({
+                    type: "GET",
+                    url: "https://api.darksky.net/forecast/ced710042ef563fc9a490fcd015bedf7/" + lat + "," + long,
+                    dataType: "jsonp"
+                }).done(function (data) {
+                    var obj = [data];
+
+                    function skycons() {
+                        var skycons = new Skycons({"color": "#fff", "resizeClear": true});
+                        skycons.add(document.getElementById("weather-icon"), obj[0].currently.icon);
+                        skycons.play();
+                    }
+                    skycons();
+                })
+            })
         });
     }
 
     function toggleTemp(event) {
         event.preventDefault();
-        $temperature.html(($temperature).html() == tempC ? tempF: tempC);
+        $temperature.html(($temperature).html() == tempCIn ? tempFIn: tempCIn);
         $switch.html(($switch).html() == "To Fahrenheit" ? "To Celsius" : "To Fahrenheit");
 
     }
-
     $switch.on("click", toggleTemp)
 });
 
