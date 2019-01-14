2b0d591a824568d6046d632ca2f94303e91851d6
Michal Muszynski <m.muszy@gmail.com>
2019-01-14 21:38:08 +0100

    Convert function

--- assets/script.js
+++ assets/script.js
@@ -56,7 +56,7 @@ function insertData(data) {
 }
 
 function convertToFahrenheit(temp) {
-  return Math.round((((temp * 9) / 5 + 32)));
+  return Math.round((temp * 9) / 5 + 32);
 }
 
 function toggleTemp() {
